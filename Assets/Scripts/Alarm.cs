764785d5dff098383c94071af57a68a7ef4dcab1
Alexandr <jimbox2001@mail.ru>
2024-03-18 14:09:09 +0300

    Fix3

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Alarm : MonoBehaviour
 {
     [SerializeField, Min(0)] private float fadeTime;
